55ba4f9c53565944ee7f4cd60f5b1ad4e0245479
happy <happy>
2019-10-04 22:37:17 +0800

    投票管理器

--- MyInport.ts
+++ MyInport.ts
@@ -3,8 +3,8 @@ import PathGameTongyong = gametongyong.data.Path;
 import TongyongClip = gametongyong.ClipUtil;
 import TongyongPageDef = gametongyong.page.TongyongPageDef;
 import RecordMgr = gametongyong.manager.RecordMgr;
+import TouPiaoMgr = gametongyong.manager.TouPiaoMgr;
 import TongyongTipsPage = gametongyong.page.TipsPage;
 import TongyongHudNqpPage = gametongyong.page.TongyongHudNqpPage;
-import TouPiaoJieSanPage = gametongyong.page.TouPiaoJieSanPage;
 import TongyongUtil = gametongyong.TongyongUtil;
 
