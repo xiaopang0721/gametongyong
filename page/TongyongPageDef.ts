50d9a921ec26febc241672f2e67dbbb0ea0ed5ba
atubo <atubo@h.com>
2019-10-23 20:04:54 +0800

    bug处理

--- page/TongyongPageDef.ts
+++ page/TongyongPageDef.ts
@@ -75,6 +75,7 @@ module gametongyong.page {
 		static myinit(str: string) {
 			super.myinit(str);
 			View.regViewRuntime(StringU.substitute("ui.{0}.game_ui.tongyong.HudUI", WebConfig.baseplatform), TongyongHudNqpPage);
+			Page.__createChildren("game_ui.tongyong.HudUI", TongyongHudNqpPage);
 			PageDef._pageClassMap[this.PAGE_TONGYONG_RECORD] = RecordPage;
 			PageDef._pageClassMap[this.PAGE_TONGYONG_TIPS] = TipsPage;
 			PageDef._pageClassMap[this.PAGE_TONGYONG_BATTER_INFO] = PaiJuInfoPage;
